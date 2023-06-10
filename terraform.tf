15957934de1e67bd53eb2dc40f96f5fd95e9aa6d
Rafael Van Belle <rafael@dataroots.io>
2023-06-10 09:28:02 +0200

    remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,17 +1,17 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "tutorial-terraform-raf"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28.0"
+      version = ">= 3.28.0"
     }
   }
 
